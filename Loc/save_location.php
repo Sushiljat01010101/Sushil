cfd56ae11a714df0766a6f8b2964d8205edfec3a
Sushiljat01010101 <mc7705191@gmail.com>
2024-08-13 22:20:32 +0530

    Update save_location.php

--- Loc/save_location.php
+++ Loc/save_location.php
@@ -1,14 +1,19 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Get latitude and longitude from POST request
     $latitude = $_POST['latitude'];
     $longitude = $_POST['longitude'];
 
-    // Creating the Google Maps link
+    // Generate Google Maps link
     $googleMapsLink = "https://www.google.com/maps?q=" . $latitude . "," . $longitude;
 
+    // Data to be saved
     $data = "Google Maps Link: " . $googleMapsLink . "\n";
+
+    // Specify the file where data will be saved
     $file = 'data.txt';
 
+    // Save the data to the file
     if (file_put_contents($file, $data, FILE_APPEND | LOCK_EX)) {
         echo "Location saved successfully!";
     } else {
